53d1bf38cf7e5158a684afa4cf650e2826a45c33
ukulelelofi <60286739+ukulelelofi@users.noreply.github.com>
2026-06-30 01:45:05 +0800

    Add API_KEY and simplify total amount print

--- src/app.py
+++ src/app.py
@@ -1,8 +1,10 @@
 print("Welcome to CloudMart Solutions!")
 
+API_KEY = "1234567890abcdef"
+
 def calculate_total(price, quantity):
     return price * quantity
 
 total = calculate_total(100, 2)
 
-print(f"Total Amount: {total}")
+print(total)
